d696fca82c781542fe0e1c78d7842fc0f40f0bba
Jason Yen <jasonyen2004@gmail.com>
2026-06-29 14:56:36 -0500

    feat: logout works now. (#24)

--- src/wurdal.py
+++ src/wurdal.py
@@ -2,10 +2,12 @@
 import asyncio
 import login_service
 import register_service
+import guess_service
 import board_service
-from session_service import load_player_session
-from utils import parse_args
+import logout_service
 
+from session_service import load_player_session 
+from utils import load_players, parse_args
 
 async def main():
     # TODO: set up loading in user session and user session management in general
@@ -14,7 +16,6 @@ async def main():
     if args.command == "register":
         await register_service.register(args.player_name)
     elif args.command == "login":
-        # TODO: hook up login function so that it calls created api
         await login_service.login(args.player_name)
     elif args.command == "guess":
         pass
@@ -22,6 +23,8 @@ async def main():
         await board_service.call_board_api(load_player_session())
     elif args.command == "leaderboard":
         pass
+    elif args.command == "logout":
+        await logout_service.logout()
 
 
 if __name__ == "__main__":
